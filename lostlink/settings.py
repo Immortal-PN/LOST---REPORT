c2b77f0dc31d7ae55ef94024c257c5233c88943e
Pratham Malkan <prathamclg7@gmail.com>
2026-03-16 01:51:51 +0530

    lostlink ready for railway deployment

--- lostlink/settings.py
+++ lostlink/settings.py
@@ -6,9 +6,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-secret-key'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    ".up.railway.app",
+    "localhost",
+    "127.0.0.1"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.up.railway.app"
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
